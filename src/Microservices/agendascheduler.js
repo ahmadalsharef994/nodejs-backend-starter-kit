f9f28ccc6490eece33dfd2fd931cb40eb32b1ce4
Mitesh Metha <miteshmehta2018@gmail.com>
2021-11-18 14:06:00 +0530

    agendaFix2

--- src/Microservices/agendascheduler.js
+++ src/Microservices/agendascheduler.js
@@ -1,4 +1,4 @@
-const agenda = require('./agendasetup');
+const agenda = require('agenda');
 
 const ScheduleSessionJob = async (appointmentID, startTime) => {
   const datetime = startTime.getTime() - 300000; // This comes appointment start time - 5 minutes, 300000 is Milisecond offset
--- src/Microservices/index.js
+++ src/Microservices/index.js
@@ -2,4 +2,5 @@ module.exports.emailService = require('./email.service');
 module.exports.smsService = require('./sms.service');
 module.exports.fileUpload = require('./fileUpload.service');
 module.exports.profilePhotoUpload = require('./profilePhotoUpload');
+module.exports.agendaScheduler = require('./agendascheduler');
 // module.exports.prescriptionUpload = require('./generatePrescription.service');

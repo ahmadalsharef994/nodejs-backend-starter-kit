71155c21de9548225ec2b594faadfa18cb3f2128
Nitesh Yadav <nitesh.yadav@medzgo.com>
2022-02-10 14:32:11 +0530

    fixed response for PrepaidOrder

--- src/controllers/labtest.controller.js
+++ src/controllers/labtest.controller.js
@@ -136,7 +136,7 @@ const bookPrepaidOrder = catchAsync(async (req, res) => {
     }
     return res.status(httpStatus.OK).json({ message: 'Success', isOrderPlaced, orderData });
   }
-  return res.status(httpStatus.OK).json({ message: 'Failed', isOrderPlaced, error: 'Order Request Failed' });
+  return res.status(httpStatus.BAD_REQUEST).json({ message: 'Order Request Failed' });
 });
 
 // not supported by thyrocare

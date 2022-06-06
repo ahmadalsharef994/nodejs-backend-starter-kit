e71fedddd3f284541e3b1ef9c72900529a182b76
ahmadalsharef994 <ahmadalsharef2021@gmail.com>
2022-06-06 16:32:47 +0530

    submit user details & rate limiter

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,18 +1,27 @@
 const rateLimit = require('express-rate-limit');
+const config = require('../config/config');
+
+let maxAuthLimiter = 10;
+let maxOTPLimiter = 3;
+
+if (config.env === 'development') {
+  maxAuthLimiter = 50;
+  maxOTPLimiter = 10;
+}
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: maxAuthLimiter,
   skipSuccessfulRequests: true,
 });
 
-const otpratelimiter = rateLimit({
+const otpLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 3,
+  max: maxOTPLimiter,
   skipSuccessfulRequests: false,
 });
 
 module.exports = {
   authLimiter,
-  otpratelimiter,
+  otpLimiter,
 };

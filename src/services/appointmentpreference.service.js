78efc467393da753b2260638d92ba890aeb7ab09
ahmadalsharef994 <ahmadalsharef2021@gmail.com>
2022-06-09 13:59:34 +0530

    appointment preferences /profile/updatePref

--- src/services/appointmentpreference.service.js
+++ src/services/appointmentpreference.service.js
@@ -1,9 +1,32 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-plusplus */
 const httpStatus = require('http-status');
 const AppointmentPreference = require('../models/appointmentPreference.model');
 const { createSlots, calculateDuration } = require('../utils/SlotsCreator');
 const ApiError = require('../utils/ApiError');
 // eslint-disable-next-line import/no-useless-path-segments
 const { doctorprofileService } = require('../services');
+
+const gap = parseInt(process.env.GAP, 10);
+const slotTime = parseInt(process.env.SLOT_TIME, 10);
+const duration = parseInt(process.env.DURATION, 10);
+const jump = parseInt(duration / slotTime, 10);
+
+const checkforDoctorPreference = async (AuthData) => {
+  const preference = await AppointmentPreference.find({ doctorAuthId: AuthData._id });
+  if (typeof preference[0] === 'object') {
+    return preference;
+  }
+  return null;
+};
+const checkForAppointmentPrice = async (AuthData) => {
+  const basicDetails = await doctorprofileService.fetchbasicdetails(AuthData);
+  if (!basicDetails || !basicDetails.appointmentPrice) {
+    return false;
+  }
+  return true;
+};
 
 const slotOverlap = (timeSlots) => {
   // input slots should be in ascending order
@@ -29,22 +52,8 @@ const slotOverlap = (timeSlots) => {
   }
   return false;
 };
-const checkforDoctorPreference = async (AuthData) => {
-  const preference = await AppointmentPreference.find({ doctorAuthId: AuthData._id });
-  if (typeof preference[0] === 'object') {
-    return preference;
-  }
-  return null;
-};
-const checkForAppointmentPrice = async (AuthData) => {
-  const basicDetails = await doctorprofileService.fetchbasicdetails(AuthData);
-  if (!basicDetails || !basicDetails.appointmentPrice) {
-    return false;
-  }
-  return true;
-};
 
-const createPreference = async (body, doctorID, AuthData, update = false) => {
+const createpreference = async (body, doctorID, AuthData, update = false) => {
   const alreadyExist = await AppointmentPreference.findOne({ docid: doctorID, doctorAuthId: AuthData });
   if (alreadyExist && !update) {
     return Promise.reject(
@@ -139,20 +148,89 @@ const createPreference = async (body, doctorID, AuthData, update = false) => {
   return result;
 };
 
-const updatePreference = async (body, doctorId, AuthData) => {
-  const result = await createPreference(body, doctorId, AuthData, true);
-  const promise = await AppointmentPreference.findOneAndUpdate(
-    { docid: doctorId, doctorAuthId: AuthData },
-    result,
-    { useFindAndModify: false, new: true },
-    (err, docs) => {
-      if (err) {
-        return err;
-      }
-      return docs;
+function judge(currentSlots, range) {
+  let postCheckFlag = -1;
+
+  for (let i = 0; i < currentSlots.length; i += jump) {
+    const currentStart = currentSlots[i];
+    const currentEnd = currentSlots[i + jump - 1];
+
+    const preCheck =
+      (currentStart.FromMinutes - 1 === 0
+        ? (currentStart.FromHour - 1) * 60 + 60
+        : currentStart.FromHour * 60 + currentStart.FromMinutes - 1) -
+      (range[3] === 0 ? (range[2] - 1) * 60 + 60 : range[2] * 60 + range[3]);
+
+    const postCheck =
+      (range[1] === 0 ? (range[0] - 1) * 60 + 60 : range[0] * 60 + range[1]) -
+      (currentEnd.ToMinutes === 0 ? (currentEnd.ToHour - 1) * 60 + 60 : currentEnd.ToHour * 60 + currentEnd.FromMinutes);
+
+    if (preCheck >= gap) {
+      if (postCheckFlag === 1 || i === 0) return { isAllowed: true, index: i };
     }
-  ).exec();
-  return promise;
+
+    // eslint-disable-next-line no-unused-expressions
+    postCheck >= gap ? (postCheckFlag = 1) : (postCheckFlag = -1);
+  }
+  // index = -1 indicates push at the end
+  return postCheckFlag === 1 || currentSlots.length === 0 ? { isAllowed: true, index: -1 } : { isAllowed: false };
+}
+
+function generateSolts(fhr, fmin) {
+  const slots = [];
+  let startMin = fmin;
+  let startHr = fhr;
+  for (let i = 0; i < jump; i++) {
+    let flag = false;
+    let endMin = startMin + slotTime;
+    let endHr = startHr;
+
+    if (endMin >= 60) {
+      endMin -= 60;
+      flag = true;
+    }
+    if (flag) endHr++;
+    const obj = { FromHour: startHr, FromMinutes: startMin + 1, ToHour: endHr, ToMinutes: endMin };
+    slots.push(obj);
+    startHr = endHr;
+    startMin = endMin;
+  }
+  return slots;
+}
+
+const updatePreference = async (body, doctorId) => {
+  const existingSlots = await AppointmentPreference.findOne({ docid: doctorId });
+  Object.keys(body).map((day) => {
+    // eslint-disable-next-line array-callback-return
+    body[day].map((range) => {
+      if (range.ToHour * 60 + range.ToMinutes - (range.FromHour * 60 + range.FromMinutes) !== duration) {
+        throw new ApiError(httpStatus.BAD_REQUEST, { message: `duration must be ${duration}` });
+      }
+      if (range.ToMinutes < gap) {
+        range.ToMinutes = 60 - gap + range.ToMinutes;
+        range.ToHour -= 1;
+      } else {
+        range.ToMinutes -= gap;
+      }
+      const resultCheck = judge(existingSlots[`${day}_A`], Object.values(range));
+
+      if (resultCheck.isAllowed) {
+        // eslint-disable-next-line no-unused-expressions
+        resultCheck.index === -1
+          ? existingSlots[`${day}_A`].push(...generateSolts(...Object.values(range).slice(0, 2)))
+          : (existingSlots[`${day}_A`] = [
+              ...existingSlots[`${day}_A`].slice(0, resultCheck.index),
+              ...generateSolts(...Object.values(range).slice(0, 2)),
+              ...existingSlots[`${day}_A`].slice(resultCheck.index),
+            ]);
+      } else {
+        throw new ApiError(httpStatus.BAD_REQUEST, { message: `slots are overlapping` });
+      }
+    });
+  });
+  // console.log(existingSlots);
+  await existingSlots.save();
+  return existingSlots;
 };
 
 const getfollowups = async (doctorId) => {
@@ -170,7 +248,8 @@ const getappointments = async (doctorId) => {
   );
   return promise;
 };
-const checkappointmentPreference = async (docid, doctorauth) => {
+
+const checkAppointmentPreference = async (docid, doctorauth) => {
   try {
     const { doctorAuthId } = await AppointmentPreference.findOne({ docid });
     if (`${doctorauth}` === `${doctorAuthId}`) {
@@ -180,13 +259,14 @@ const checkappointmentPreference = async (docid, doctorauth) => {
     return false;
   }
 };
+
 module.exports = {
   checkForAppointmentPrice,
-  createPreference,
+  createpreference,
   updatePreference,
   getfollowups,
   getappointments,
   slotOverlap,
   checkforDoctorPreference,
-  checkappointmentPreference,
+  checkAppointmentPreference,
 };

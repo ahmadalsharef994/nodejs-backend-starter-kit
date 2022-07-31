c9e086c57a3c68ac58d610776c8f5894e1207361
ahmadalsharef994 <ahmadalsharef2021@gmail.com>
2022-07-31 18:02:31 +0530

    fix/chat_controller

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,36 +1,18 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
-// const pick = require('../utils/pick');
 const { chatService, authService } = require('../services');
 const ApiError = require('../utils/ApiError');
 
 const getMessages = catchAsync(async (req, res) => {
   const AuthData = await authService.getAuthById(req.SubjectId);
-  // const filter = { appointment: req.params.appointmentId };
-  // const options = pick(req.query, ['sortBy', 'limit', 'page']);
+
   await chatService.getMessages(req.params.appointmentId, AuthData).then((result, err) => {
     if (err) {
       throw new ApiError(httpStatus.BAD_REQUEST, 'Not Authorized to Access this Chat');
-    } else if (result.length === 0) {
-      return res.status(httpStatus.OK).json({ message: 'No Messages Found', data: [] });
     }
     return res.status(httpStatus.OK).json({ message: 'Success', data: result });
   });
 });
-
-// const sendMessage = catchAsync(async (req, res) => {
-//   if (!req.params.appointmentId === req.appointment) {
-//     return res.status(httpStatus.BAD_REQUEST).json({ message: 'Not Authorized to Access this Chat', data: [] });
-//   }
-//   if (req.entity === 'doctor') {
-//     await chatService.createMessage(req.appointment, req.doctor, req.body.text, req.body.attachment);
-//     return res.status(httpStatus.OK).json({ message: 'Success' });
-//   }
-//   if (req.entity === 'user') {
-//     await chatService.createMessage(req.appointment, req.user, req.body.text, req.body.attachment);
-//     return res.status(httpStatus.OK).json({ message: 'Success' });
-//   }
-// });
 
 module.exports = {
   getMessages,

0138f54fcfdabc4e0733b22b6121c57674fe9811
ahmadalsharef994 <ahmadalsharef994@gmail.com>
2025-07-09 15:50:31 +0200

    none

--- src/Microservices/index.js
+++ src/Microservices/index.js
@@ -3,6 +3,6 @@ module.exports.smsService = require('./sms.service');
 module.exports.fileUpload = require('./fileUpload.service');
 module.exports.profilePhotoUpload = require('./photoUpload.service');
 // module.exports.thyrocareServices = require('./thyrocareServices');
-module.exports.razorpayPaymentServices = require('./razorpay.service');
+module.exports.razorpayPaymentServices = require('./payment.service');
 module.exports.prescriptionUpload = require('./generatePrescription.service');
 module.exports.elasticSearchService = require('./elasticSearch.service');
